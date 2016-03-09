6c794ba2f15e3ce3d3f5e8b92d7fcaa41a6271c8
Alexey <ximagination80@gmail.com>
2016-03-10 00:11:40 +0200

    Transaction

--- app/dao/DefaultDAO.scala
+++ app/dao/DefaultDAO.scala
@@ -1,14 +1,13 @@
 package dao
 
 import play.api.db.slick.HasDatabaseConfigProvider
+import play.api.libs.concurrent.Execution.defaultContext
 import slick.driver.H2Driver.api._
 import slick.driver.JdbcProfile
 import slick.lifted.{Rep, TableQuery}
 
-import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.Future.successful
-
-import play.api.libs.concurrent.Execution.defaultContext
+import scala.concurrent.{ExecutionContext, Future}
 
 trait DefaultDAO[ID, E, Z <: Table[E]] extends BaseDAO[ID, E] with HasExecutionContext {
   this: HasDatabaseConfigProvider[JdbcProfile] =>
@@ -21,28 +20,28 @@ trait DefaultDAO[ID, E, Z <: Table[E]] extends BaseDAO[ID, E] with HasExecutionC
   // @formatter:on
 
   def insertSeq(e: Seq[E]): Future[Seq[ID]] =
-    if (e.nonEmpty) db.run((q returning id()) ++= e) else successful(Seq.empty[ID])
+    if (e.nonEmpty) db.run(((q returning id()) ++= e).transactionally) else successful(Seq.empty[ID])
 
   def insertForce(e: Seq[E]): Future[Unit] =
-    if (e.nonEmpty) db.run(q ++= e) map (_ => ()) else successful((): Unit)
+    if (e.nonEmpty) db.run((q ++= e).transactionally) map (_ => ()) else successful((): Unit)
 
   def delete(): Future[Int] =
-    db.run(q.delete)
+    db.run(q.delete.transactionally)
 
   def delete(ids: Seq[ID]): Future[Int] =
-    if (ids.nonEmpty) db.run(filterIds(ids).delete) else successful(0)
+    if (ids.nonEmpty) db.run(filterIds(ids).delete.transactionally) else successful(0)
 
   def update(e: E): Future[Int] =
-    getId(e).fold(successful(0))(id => db.run(filterIds(id :: Nil).update(e)))
+    getId(e).fold(successful(0))(id => db.run(filterIds(id :: Nil).update(e).transactionally))
 
   def seq(offset: Int, limit: Int): Future[Seq[E]] =
-    db.run(q.drop(offset).take(limit).result)
+    db.run(q.drop(offset).take(limit).result.transactionally)
 
   def byId(e: Seq[ID]): Future[Seq[E]] =
-    db.run(filterIds(e).result)
+    db.run(filterIds(e).result.transactionally)
 
   def count(): Future[Int] =
-    db.run(q.countDistinct.result)
+    db.run(q.countDistinct.result.transactionally)
 
   def whereOne(f: Z => Rep[Boolean]): Future[E] =
     whereSeq(f) map (_.head)
@@ -51,7 +50,7 @@ trait DefaultDAO[ID, E, Z <: Table[E]] extends BaseDAO[ID, E] with HasExecutionC
     whereSeq(f) map (_.headOption)
 
   def whereSeq(f: Z => Rep[Boolean]): Future[Seq[E]] =
-    db.run(q.filter(f).result)
+    db.run(q.filter(f).result.transactionally)
 
   // play execution context by default
   implicit def executionContext: ExecutionContext = defaultContext
